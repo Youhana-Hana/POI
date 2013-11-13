af00c3af2cc632c44116782bb7af8ed719c52dea
Youhana-Hana <meyouhana@gmail.com>
2013-11-13 20:02:43 +0000

    [#60476818] exclude coverage folder

--- test/coverage.sh
+++ test/coverage.sh
@@ -1,5 +1,7 @@
 set -e
 
+rm -r ./coverage
+
 ./node_modules/istanbul/lib/cli.js cover ./node_modules/mocha/bin/_mocha -- -R spec ./test/*.js
 
 ./node_modules/istanbul/lib/cli.js check-coverage --statement 100
